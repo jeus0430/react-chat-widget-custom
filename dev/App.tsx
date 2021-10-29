4b63fcf721ed13225831a616c5733ebaf4e08e46
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-29 10:57:57 +0900

    first commit

---
+++ dev/App.tsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react"
+
+import {
+  Widget,
+  addResponseMessage,
+  setQuickButtons,
+  toggleMsgLoader,
+  addLinkSnippet,
+  dropMessages,
+  addUserMessage,
+} from "../index"
+// import { addUserMessage } from ".."
+
+export default class App extends Component {
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!")
+    addLinkSnippet({ link: "https://google.com", title: "Google" })
+    addResponseMessage(
+      "![](https://raw.githubusercontent.com/Wolox/press-kit/master/logos/logo_banner.png)",
+      "Vaishali2"
+    )
+    addResponseMessage(
+      "![vertical](https://d2sofvawe08yqg.cloudfront.net/reintroducing-react/hero2x?1556470143)",
+      "Vaishai22"
+    )
+  }
+
+  handleNewUserMessage = (newMessage: any) => {
+    toggleMsgLoader()
+    setTimeout(() => {
+      toggleMsgLoader()
+      if (newMessage === "fruits") {
+        setQuickButtons([
+          { label: "Apple", value: "apple" },
+          { label: "Orange", value: "orange" },
+          { label: "Pear", value: "pear" },
+          { label: "Banana", value: "banana" },
+        ])
+      } else {
+        addResponseMessage(newMessage, "Vaishali")
+      }
+    }, 2000)
+  }
+
+  handleQuickButtonClicked = (e: any) => {
+    addResponseMessage("Selected " + e, "Vaishali")
+    setQuickButtons([])
+  }
+
+  handleSubmit = (msgText: string) => {
+    if (msgText.length < 10)
+      addUserMessage("Uh oh, please write a bit more.")
+    else
+      addUserMessage(msgText, "Me")
+    return false
+    // return true
+  }
+
+  render() {
+    return (
+      <div>
+        <button style={{ position: "absolute", right: 40, bottom: 150 }}>
+          test
+        </button>
+        <Widget
+          title="Bienvenido"
+          subtitle="Asistente virtual"
+          senderPlaceHolder="Escribe aquí ..."
+          handleNewUserMessage={this.handleNewUserMessage}
+          handleQuickButtonClicked={this.handleQuickButtonClicked}
+          imagePreview
+          handleSubmit={this.handleSubmit}
+        />
+      </div>
+    )
+  }
+}
---
+++ src/store/dispatcher.ts
@@ -0,0 +1,65 @@
+import { ElementType } from 'react';
+
+import store from '.';
+import * as actions from './actions';
+import { LinkParams, ImageState } from './types';
+
+export function addUserMessage(text: string, name?: string, id?: string) {
+  store.dispatch(actions.addUserMessage(text, name, id));
+}
+
+export function addResponseMessage(text: string, name?: string, id?: string) {
+  store.dispatch(actions.addResponseMessage(text, name, id));
+}
+
+export function addLinkSnippet(link: LinkParams, name?: string, id?: string) {
+  store.dispatch(actions.addLinkSnippet(link, name, id));
+}
+
+export function toggleMsgLoader() {
+  store.dispatch(actions.toggleMsgLoader());
+}
+
+export function renderCustomComponent(component: ElementType, props: any, showAvatar = false, id?: string) {
+  store.dispatch(actions.renderCustomComponent(component, props, showAvatar, id));
+}
+
+export function toggleWidget() {
+  store.dispatch(actions.toggleChat());
+}
+
+export function toggleInputDisabled() {
+  store.dispatch(actions.toggleInputDisabled());
+}
+
+export function dropMessages() {
+  store.dispatch(actions.dropMessages());
+}
+
+export function isWidgetOpened(): boolean {
+  return store.getState().behavior.showChat;
+}
+
+export function setQuickButtons(buttons: Array<{ label: string, value: string | number }>) {
+  store.dispatch(actions.setQuickButtons(buttons));
+}
+
+export function deleteMessages(count: number, id?: string) {
+  store.dispatch(actions.deleteMessages(count, id));
+}
+
+export function markAllAsRead() {
+  store.dispatch(actions.markAllMessagesRead());
+}
+
+export function setBadgeCount(count: number) {
+  store.dispatch(actions.setBadgeCount(count));
+}
+
+export function openFullscreenPreview(payload: ImageState) {
+  store.dispatch(actions.openFullscreenPreview(payload));
+}
+
+export function closeFullscreenPreview() {
+  store.dispatch(actions.closeFullscreenPreview());
+}
